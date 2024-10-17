16a1d166e3ca5b2c2328c5aba6723dc3c2089660
😈Sviatoslav🤑 <132163617+sviatoslav-shubnikov@users.noreply.github.com>
2024-10-17 23:29:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-string = input("Введите строку для изменений 1-ого и последнего символа: ")
+string = input("Введите строку для изменений 1-ого и последнего символа: ") # Получаем строку от пользователя через ввод
 
-lines = string.split(' ')
-modified_lines = []
+lines = string.split(' ') # Разбиваем строку на слова, используя пробелы как разделители
+modified_lines = [] # Создаем пустой список для хранения модифицированных слов
 
-for line in lines:
+for line in lines: # Перебираем каждое слово в списке lines
     
-    if len(line) > 1:
+    if len(line) > 1: # Если длина слова больше 1 символа (чтобы избежать изменения однобуквенных слов)
         
-        modified_line = line[-1] + line[1:-1] + line[0]
+        modified_line = line[-1] + line[1:-1] + line[0] # Меняем первый и последний символы местами
         
-    else:
+    else: # Если условие не верно, выполняется блок_инструкций ниже
         
-        modified_line = line
+        modified_line = line # Приравниваем к modified_line, оставляя его как есть
         
-    modified_lines.append(modified_line)
+    modified_lines.append(modified_line) # Добавляем модифицированное слово в список modified_lines
     
-modified_text = ' '.join(modified_lines)
+modified_text = ' '.join(modified_lines) # Объединяем модифицированные слова в строку, разделяя их пробелами
 
-print(f"Измененный текст: {modified_text}")
+print(f"Измененный текст: {modified_text}") # Выводим модифицированную строку
